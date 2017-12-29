5b96901b026999636e45b68eb0e53311e7d18a89
Avi Brenner <abrenner@salesforce.com>
2017-12-28 20:51:23 -0800

    Initial commit with lwgl work

---
+++ src/renderEngine/DisplayManager.java
@@ -0,0 +1,35 @@
+package renderEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+
+public class DisplayManager {
+    private static int WIDTH = 1280;
+    private static int HEIGHT = 720;
+    private static int FPS_CAP = 60;
+
+    public static void createDisplay() {
+        ContextAttribs attribs = new ContextAttribs(3,2)
+                .withForwardCompatible(true)
+                .withProfileCore(true);
+
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create(new PixelFormat(), attribs);
+            Display.setTitle("Game Playground");
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+
+        GL11.glViewport(0,0, WIDTH, HEIGHT);
+    }
+
+    public static void updateDisplay() {
+        Display.sync(FPS_CAP);
+        Display.update();
+    }
+
+    public static void closeDisplay() {
+        Display.destroy();
+    }
+}
